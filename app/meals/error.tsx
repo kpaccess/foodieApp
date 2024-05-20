368728b00d4281932532dfffcdc673a4ffe0dfd0
krishna pradhan <kpaccess@gmail.com>
2024-05-20 17:41:37 -0400

    showing error page

---
+++ app/meals/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+const Error = ({ error }: { error: Error }) => {
+  // text-transparent bg-clip-text bg-gradient-to-r from-[#f9572a] to-[#ff8a05]
+  return (
+    <main className=" mt-20 text-center">
+      <h1 className=" text-7xl m-0 font-black  uppercase text-transparent bg-clip-text  bg-gradient-to-r from-[#f9572a] to-[#ffc905] bg-center font-sans">
+        An Error occured - {error.message}
+      </h1>
+      <p className=" text-2xl font-medium text-[#ddd8d8] mt-3">
+        Failed to fetch meal data. Please try again later.
+      </p>
+    </main>
+  );
+};
+
+export default Error;
